fbcf870f69ba957c19913d88408c8ab8c5af29bd
Ansarul Mullah <mdansarul543@gmail.com>
2024-02-01 20:18:17 +0600

    okkkk

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image ,View} from "react-native";
+import { Image, View, Button, TouchableOpacity, Alert } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ioniccons from "react-native-vector-icons/Ionicons"
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -17,19 +17,19 @@ function LogoTitle() {
     <View style={{display:"flex",flex:1,flexDirection:"column"}}>
       <Image
         style={{ width: 50,
-          height: 50,
+          height: 30,
           display: 'flex',
           flex:1,
           alignItems: 'center',
           backgroundColor: '#fff',
-          borderRadius: 70,}}
+          }}
         source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}}
       />
     </View>
   );
 };
 
-export default function MainContainer(){
+export default function MainContainer({navigation}){
   return(
     <Tab.Navigator initialRouteName="Home"
                    screenOptions={({ route }) => ({
@@ -61,7 +61,19 @@ export default function MainContainer(){
       <Tab.Screen name={"Home"} component={HomeScreen}
                   options={{headerShown:true,
                     headerTitle: (props) => <LogoTitle {...props} />,
-                    tabBarIcon:()=>{return <FontAwesome5 name={'home'}  size={20} solid />}
+                    tabBarIcon:()=>{return <FontAwesome5 name={'home'}  size={20} solid />},
+                    headerRight: () => (
+                      <TouchableOpacity>
+                      <FontAwesome5 name={'user'} onPress={()=>navigation.navigate("Login")}
+                                    color="black" style={{paddingRight:15}} size={25} solid />
+                      </TouchableOpacity>
+                    ),
+                    headerLeft: () => (
+                      <TouchableOpacity>
+                        <FontAwesome5 name={'bars'} onPress={()=>alert("Left Menu")}
+                                      color="black" style={{paddingLeft:15}} size={25}  />
+                      </TouchableOpacity>
+                    ),
                   }} />
       <Tab.Screen name={"Details"} component={DetailsScreen} options={{headerShown:true,
         tabBarIcon:()=>{return <FontAwesome5 name={'book'} size={20} solid />}
